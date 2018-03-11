c8f24fafad85f9dff0c638e6a990588df7b6a78c
Travis Clarkson <tk1667@gmail.com>
2018-03-11 23:28:46 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,4 @@
-class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.2]
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
 
   change_column :students, :birthdate, :datetime
 
